a21d18bd93bc07b9f91c0090b4912e2637927362
Yahya ElAraby <yaya1426@gmail.com>
2026-05-06 18:21:04 +0300

    Initial

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <Button variant="outline">Click me</Button>
+    </div>
+  );
+}
